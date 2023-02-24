e0bdb783f2e8a8be00b0ca5e2b9f4134fa08d587
hovhannes84 <ovojan84@gmail.com>
2023-02-24 20:58:31 +0400

    Person

---
+++ src/chepter10/ThrowsDemo.java
@@ -0,0 +1,16 @@
+package chepter10;
+
+public class ThrowsDemo {
+    static void throwOne() throws IllegalAccessException {
+        System.out.println("B теле метода throwOne() .");
+        throw new IllegalAccessException("дeмoнcтpaция");
+    }
+
+    public static void main(String[] args) {
+        try {
+            throwOne();
+        }catch (IllegalAccessException e){
+            System.out.println("Пepexвaчeнo исключение: " + e);
+        }
+    }
+}
