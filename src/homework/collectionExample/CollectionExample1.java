8f981f039d4fe54d3fc406157034e0746e82566a
hovhannes84 <ovojan84@gmail.com>
2023-03-11 12:17:58 +0400

    chepter14

---
+++ src/homework/collectionExample/CollectionExample1.java
@@ -0,0 +1,90 @@
+package homework.collectionExample;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CollectionExample1 {
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<String> nameList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        nameList.add("Aram");
+        nameList.add("Vazgen");
+        nameList.add("Karen");
+        nameList.add("Vahan");
+        nameList.add("Arsen");
+        try {
+            boolean isRun = true;
+            while (isRun) {
+                System.out.println("0 for exit");
+                System.out.println("1 for to find out if there is a name");
+                System.out.println("2 for to delete the entered name");
+                System.out.println("3 for to save the name in the visible index");
+                String comands = scanner.nextLine();
+                switch (comands) {
+                    case "0":
+                        isRun = false;
+                        break;
+                    case "1":
+                        one();
+                        break;
+                    case "2":
+                        two();
+                        break;
+                    case "3":
+                        three();
+                        break;
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("please enter the correct command");
+        }
+
+
+    }
+
+    private static void three() {
+        System.out.println("Enter your name,index");
+        String cons = scanner.nextLine();
+        String[] str = cons.split(",");
+        nameList.add(Integer.parseInt(str[1]), str[0]);
+        for (String s : nameList) {
+            System.out.println(s);
+        }
+
+
+    }
+
+    private static void two() {
+        System.out.println("Enter your name:");
+        String cons = scanner.nextLine();
+        if (nameList.contains(cons)) {
+            nameList.remove(cons);
+        } else {
+            System.out.println("this name was not found");
+        }
+
+
+        for (String s : nameList) {
+            System.out.println(s);
+        }
+
+
+    }
+
+    private static void one() {
+        System.out.println("Enter your name:");
+        String cons = scanner.nextLine();
+
+        if (nameList.contains(cons)) {
+            System.out.println("this name found");
+        } else {
+            System.out.println("this name was not found");
+        }
+
+    }
+
+}
---
+++ src/homework/collectionExample/CollectionExample2.java
@@ -0,0 +1,72 @@
+package homework.collectionExample;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class CollectionExample2 {
+    static Scanner scanner = new Scanner(System.in);
+    static TreeSet<String> nameTree = new TreeSet<>();
+    static HashSet<String> nameHash = new HashSet<>();
+
+    public static void main(String[] args) {
+        boolean isRun = true;
+        while (isRun){
+            System.out.println("0 for exit");
+            System.out.println("1 for add names");
+            System.out.println("2 for add names");
+            String comands = scanner.nextLine();
+            try {
+                switch (comands){
+                    case "0":
+                        isRun = false;
+                        break;
+                    case "1":
+                        one();
+                        break;
+                    case "2":
+                        two();
+                        break;
+                }
+            }catch (Exception e){
+                System.out.println("please enter the correct command");
+            }
+
+
+        }
+
+
+
+
+    }
+
+    private static void two() {
+        System.out.println("enter 10 yin to get sorted list of names");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("please Enter name "+ (i+1) +" to save");
+            String name = scanner.nextLine();
+            nameTree.add(name);
+        }
+        for (String s : nameTree) {
+            System.out.println(s);
+
+        }
+
+    }
+
+    private static void one() {
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("piease enter june "  + (i+1) + " st from pita");
+            String name = scanner.nextLine();
+            if (!nameHash.contains(name)) {
+                nameHash.add(name);
+            }
+
+        }
+        for (String hash : nameHash) {
+            System.out.println(hash);
+
+        }
+
+    }
+}
