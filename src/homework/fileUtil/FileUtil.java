90ebc5071ab3d9665b19b975a76aaceebd254637
hovhannes84 <ovojan84@gmail.com>
2023-03-16 18:32:34 +0400

    fileutil

--- src/homework/fileUtil/FileUtil.java
+++ src/homework/fileUtil/FileUtil.java
@@ -102,6 +102,7 @@ public class FileUtil {
         int size = getFolderSize(folder);
         System.out.println("summing file sizes" + size);
 
+
     }
 
     private static int getFolderSize(File file) {
